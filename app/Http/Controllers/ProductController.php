95d620623000b7cfe2d68c2ce4024fa693a1ee19
riyAdHmobin <riyadhmobin@gmail.com>
2022-02-10 16:28:05 +0600

    slider added in landing page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     //
     function index()
     {
-        return "Welcome To Product Page";
+        $data=Product::all();
+        return view('product',['products'=>$data]);
     }
 }
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -25,6 +25,15 @@
         height: 500px;
         padding-top: 100px;
     }
+    img.slider-img{
+        height: 400px !important;
+    }
+    .custom-product{
+        height: 600px;
+    }
+    .slider-text{
+        background: #2c462c61;
+    }
 </style>
 
 </html>
