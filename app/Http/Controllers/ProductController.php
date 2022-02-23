8bdadfdfcc202516af6c9576b1c3bd8cfb5c6749
riyAdHmobin <riyadhmobin@gmail.com>
2022-02-23 15:39:50 +0600

    remove from cart option added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -64,5 +64,11 @@ class ProductController extends Controller
 
         return view('cartlist',['products'=>$products]);
     }
+
+    function removeCart($id)
+    {
+        Cart::destroy($id);
+        return redirect('cartlist');
+    }
     
 }
